361de512b2f7c0eb8b6df5963c860b4474abd665
Marcin Tatarski <martin.tatarski>
2024-06-26 13:35:52 +0100

    Initial setup for Automated Emails Sender

---
+++ 2_Automating/email-sender-with-scheduling/main.py
@@ -0,0 +1,14 @@
+"""
+Description: Create a script that sends automated emails at scheduled times. 
+
+Steps:
+1 - setup the stmp server
+2 - email content builder
+3 - user input handling
+4 - method for scheduling emails
+5 - handling any errors
+6 - any additional helper methods
+
+Concepts: Networking, file I/O, Scheduling, Security
+
+"""
