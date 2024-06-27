1b2cadde717764f7297d48905685c70cb7dcd66d
Marcin Tatarski <martin.tatarski>
2024-06-27 22:30:22 +0100

    File rename with Undo option init setup

---
+++ 1_Utilities/file-rename-with-undo/main.py
@@ -0,0 +1,15 @@
+"""
+Description: Rename multiple files in a directory based on specified patterns, with an option to undo the renaming operation.
+
+Steps:
+1 - display interactive command-line prompts, using InquirerPy lib
+2 - load/save logs from/to *.json file
+3 - rename files in a specified directory
+4 - list logs based on timestamp from *.json file
+5 - undo renamed files
+
+Concepts: file I/O, data structures, error handling, libs
+"""
+
+
+
